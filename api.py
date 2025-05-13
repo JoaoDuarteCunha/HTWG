41c5c8cf0e61daf83c91b6ce3830997f235f4c2a
JoaoDuarteCunha <joao.ddcunha@gmail.com>
2025-05-13 10:37:57 -0400

    Update api.py

--- api.py
+++ api.py
@@ -9,7 +9,8 @@ async def root():
     message = messages.pop(0)
     return message
 
-@app.post("/send/{message}")
+@app.post("/send/")
 async def create_item(message: str):
+    print(message)
     messages.append(message)
     return message
